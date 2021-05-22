c473626fafcb5733af33ee66a2cd099a21765b3c
EllieShilla <nosova.alina97@gmail.com>
2021-05-22 10:13:30 +0300

    22
    22

---
+++ src/Othello.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+
+export default class Othello extends React.Component{
+    render(){
+      return(
+        <div className="Othello">
+          <h2>Оте́лло, венециа́нский мавр</h2>
+          <p>Пьеса «Отелло» Шекспира была написана в 1604 году. За основу сюжета трагедии автор взял произведение Джиральди 
+            Чинтио «Венецианский мавр». Это печальная история об утраченном доверии, любви, смысле жизни.</p>
+        </div>
+      )
+    }
+  }
